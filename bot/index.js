c8b89788ac37bd4dd50f31c8a6a371dd1a7a487a
unknown <bekmurodgofuorv0000@gmail.com>
2024-09-29 09:51:54 -0700

    send data and fix price

--- bot/index.js
+++ bot/index.js
@@ -3,13 +3,44 @@ const token = "7811968929:AAFEExjXm-yOuIPwIul0JQFoEjUzbBHpDQQ";
 
 const bot = new TelegramBot(token, {polling: true});
 
+
 const bootstrap = () => {
     bot.on('message', async msg => {
         const chatId = msg.chat.id
         const text = msg.text
 
         if (text == "/start") {
-            await bot.sendMessage(chatId, `Salom ${msg.from.first_name} botga hush kelibsiz😊`)
+            await bot.sendMessage(chatId, `Salom ${msg.from.first_name} botga hush kelibsiz😊`, 
+                {
+                    reply_markup: {
+                        resize_keyboard: true,
+                        keyboard: [
+                            [{text: "Click On", web_app: {url: "https://node-telegram-web-bot.vercel.app"}}]
+                        ]
+                    }
+                }
+            )
+        }
+
+        if (msg.web_app_data?.data) {
+            try {
+                const data = JSON.parse(msg.web_app_data?.data)
+
+                await bot.sendMessage(chatId, "Bizga ishonch bildirganingiz uchun rahmat, siz sotib olgan mevalar ro'yhati.")
+
+                for (item of data) {
+                    await bot.sendMessage(chatId, `${item.title} - ${item.quontity}X`)
+                }
+
+                await bot.sendMessage(chatId,
+                    `Umumiy narx: ${data.reduce((a, c)=> a + c.price * c.quontity, 0).toLocaleString("en-US", {
+                        style: "currency",
+                        currency: "USD"
+                    })}`
+                )
+            } catch (error) {
+                return bot.sendMessage(chatId, error)
+            }            
         }
     })
 }
--- web/src/constants/db.js
+++ web/src/constants/db.js
@@ -2,49 +2,49 @@ export function getData() {
     return [
         {
             title: "Olma",
-            price: 19.99,
+            price: 5.99,
             Image: "https://main-cdn.sbermegamarket.ru/big1/hlr-system/412/054/595/819/171/4/100033206141b0.jpg",
             id: 1
         },
         {
             title: "Shaftoli",
-            price: 16.99,
+            price: 3.99,
             Image: "https://i.pinimg.com/736x/ad/89/a4/ad89a4d0cc56323a5903408a48595917.jpg",
             id: 2
         },
         {
             title: "Nok",
-            price: 23.99,
+            price: 2.99,
             Image: "https://avatars.mds.yandex.net/get-eda/3581687/40e25582389c4007bd8b16c31d504ff3/1200x1200nocrop",
             id: 3
         },
         {
             title: "Siliva",
-            price: 18.99,
+            price: 1.99,
             Image: "https://apeti.ru/upload/iblock/80b/bhelbruz92ni8tpimeg61hc55abdtjl6/sliva_chernosliv_moldova.jpg",
             id: 4
         },
         {
             title: "Anor",
-            price: 26.99,
+            price: 3.99,
             Image: "https://main-cdn.sbermegamarket.ru/big1/hlr-system/126/125/876/611/181/121/100029578088b0.jpg",
             id: 5
         },
         {
             title: "O'rik",
-            price: 22.99,
+            price: 2.99,
             Image: "https://new-s.com.ua/wp-content/uploads/2017/03/66.jpg",
             id: 6
         },
         {
             title: "Giloz",
-            price: 30.99,
+            price: 4.99,
             Image: "https://i.pinimg.com/736x/e0/69/10/e0691075395140aa1660f543ba2c1fcb.jpg",
             id: 7
         },
         {
             title: "Behi",
-            price: 15.99,
+            price: 4.99,
             Image: "https://main-cdn.sbermegamarket.ru/big2/hlr-system/427/474/362/325/184/4/100028179769b0.jpg",
             id: 8
         },
